63d16403976698ed983af4db080d74e73c845892
angel_archbold <angelarchbold09@gmail.com>
2021-03-18 23:22:46 -0500

    Server terminado

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
   
 require('dotenv').config()
 const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8000,
   cors: process.env.CORS,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
